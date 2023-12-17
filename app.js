67eed3d5b55adfab3692c0e318edba50d6bc47f5
Ade Suluh N <ade.suluh@gmail.com>
2023-12-17 21:26:35 +0700

    start empty

--- app.js
+++ app.js
@@ -20,38 +20,5 @@ qapmInit();
 //app.js
 App({
   onLaunch: function () {
-    wx.setTabBarItem({
-  index: 0,
-  text: 'text',
- 
-})
-    // show localstorage
-    let logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // login
-    wx.login({
-      success: res => {
-        // get custom login info res
-      }
-    })
-    // get user settings
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // Already authorized, you can directly call getUserInfo to get the avatar nickname without popping up
-          wx.getUserInfo({
-            success: res => {
-              // get custom user info res
-              this.globalData.userInfo = res.userInfo;
-            }
-          })
-        }
-      }
-    })
   },
-  globalData: {
-    userInfo: null
-  }
 })
